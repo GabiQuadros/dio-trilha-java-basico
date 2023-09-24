6d0b0ee609e6b7acaca10b07f68dc3b01ca8bce8
GabiQuadros <ngabiquadros@gmail.com>
2023-09-24 02:07:29 -0300

    estruturas de repetição

--- tipos-variaveis/src/edu/gabi/estruturasDeRepeticao/ExemploForArray.java
+++ tipos-variaveis/src/edu/gabi/estruturasDeRepeticao/ExemploForArray.java
@@ -1,24 +1,18 @@
 package edu.gabi.estruturasDeRepeticao;
 
 public class ExemploForArray {
-    // public static void main(String[] args) {
-    //     String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS"};
+    public static void main(String[] args) {
+        String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS"};
 
-    //     for (int x=0; x<alunos.length; x++){
-    //         System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
-    //     }
+        for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+        }
 
-    //     for(String aluno : alunos){
-    //         System.out.println("Nome do aluno é:" + aluno);
-    //     }
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é:" + aluno);
+        }
 
         
-    // }
-    public static void main(String[] args) {
-        boolean condicao = false;
-
-        while(condicao){
-            System.out.println("executou ... ");
-        }
     }
+    
 }
